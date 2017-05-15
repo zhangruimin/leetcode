f9c7e287078fceba063579f015e6db064a98baa6
Ruimin, Zhang <zrruimin@amazon.com>
2017-05-15 22:08:22 +0800

    leetcode

---
+++ src/L2017051504_553_Optimal_Division.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ */
+public class L2017051504_553_Optimal_Division {
+    public String optimalDivision(int[] nums) {
+        if (nums.length == 1) {
+            return String.valueOf(nums[0]);
+        }
+        if (nums.length == 2) {
+            return Arrays.stream(nums).boxed().map(String::valueOf).collect(Collectors.joining("/"));
+        }
+        return nums[0]+"/("+getSecondPart(nums)+")";
+    }
+
+    private String getSecondPart(int[] nums) {
+        return Arrays.stream(nums).skip(1).boxed().map(String::valueOf).collect(Collectors.joining("/"));
+    }
+}
+
+
+
+
+
