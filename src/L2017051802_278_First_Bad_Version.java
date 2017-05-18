a5e57c134eb22e12a1b40354af274bd100ae5fd4
Ruimin, Zhang <zrruimin@amazon.com>
2017-05-18 10:33:36 +0800

    leetcode

---
+++ src/L2017051802_278_First_Bad_Version.java
@@ -0,0 +1,29 @@
+/**
+ */
+public class L2017051802_278_First_Bad_Version {
+
+    class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int left = 1;
+            int right = n;
+            while (left <= right) {
+                int mid = left + (right - left) / 2; //(left+high)/2 will cause overflow
+                if (isBadVersion(mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+
+
+    }
+
+    class VersionControl {
+        boolean isBadVersion(int mid) {
+            return false;
+        }
+    }
+}
