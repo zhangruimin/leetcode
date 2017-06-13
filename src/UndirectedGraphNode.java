d2511d9efb769dda26a1c76edab4a7893eaa6bcc
Ruimin, Zhang <zrruimin@amazon.com>
2017-06-13 10:36:39 +0800

    leetcode

---
+++ src/UndirectedGraphNode.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UndirectedGraphNode {
+    int label;
+    public List<UndirectedGraphNode> neighbors;
+
+    UndirectedGraphNode(int x) {
+        label = x;
+        neighbors = new ArrayList<UndirectedGraphNode>();
+    }
+}
