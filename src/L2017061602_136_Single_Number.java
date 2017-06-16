75429bc8304a79bf84e6f97cb96cae61995c9e50
Ruimin, Zhang <zrruimin@amazon.com>
2017-06-16 09:59:32 +0800

    leetcode

---
+++ src/L2017061602_136_Single_Number.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ */
+public class L2017061602_136_Single_Number {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^=  num;
+        }
+        return result;
+    }
+}
+
