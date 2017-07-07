f8bf70bd18b775dbfbf2683d1ff0603d9069b481
Ruimin, Zhang <zrruimin@amazon.com>
2017-07-07 10:22:02 +0800

    leetcode

---
+++ src/L2017070601_24_Swap_Nodes_in_Pairs.java
@@ -0,0 +1,36 @@
+/**
+ */
+public class L2017070601_24_Swap_Nodes_in_Pairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode first = head;
+        ListNode result = null;
+        ListNode tail = null;
+
+        while (first != null) {
+            if (first.next != null) {
+                ListNode second = first.next;
+                ListNode temp = second.next;
+                second.next = first;
+                first.next = null;
+                first = temp;
+                if (result == null) {
+                    result = second;
+                } else {
+                    tail.next.next = second;
+                }
+                tail = second;
+            } else {
+                tail.next.next = first;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
+
